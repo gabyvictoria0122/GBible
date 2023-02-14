cc6e9c84b26432723f55e06b42d700cc5cbe525a
Gabriela Victoria <gabrielavictoria91@gmail.com>
2023-02-14 13:46:10 -0300

    Enchendo linguiça

--- frontend/pages/more.vue
+++ frontend/pages/more.vue
@@ -1,11 +1,103 @@
 <template>
-  <h1>HEEYYYY AQUI É O MAIS</h1>
+  <v-row>
+    <v-col
+      cols="12"
+      sm="6"
+      offset-sm="3"
+    >
+      <v-card>
+        <v-list>
+          <template v-for="(item, index) in items">
+            <v-list-item
+              v-if="item.action"
+              :key="item.title"
+            >
+              <v-list-item-action>
+                <v-icon>{{ item.action }}</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-divider
+              v-else-if="item.divider"
+              :key="index"
+            />
+
+            <v-subheader
+              v-else-if="item.header"
+              :key="item.header"
+            >
+              {{ item.header }}
+            </v-subheader>
+          </template>
+        </v-list>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
-  layout: 'complex'
+  layout: 'complex',
+  data () {
+    return {
+      items: [
+        {
+          action: 'mdi-inbox-arrow-down',
+          title: 'Inbox'
+        },
+        {
+          action: 'mdi-send',
+          title: 'Sent'
+        },
+        {
+          action: 'mdi-inbox-arrow-down',
+          title: 'Inbox'
+        },
+        {
+          action: 'mdi-send',
+          title: 'Sent'
+        },
+        {
+          action: 'mdi-delete',
+          title: 'Trash'
+        },
+        {
+          action: 'mdi-alert-octagon',
+          title: 'Spam'
+        },
+        { divider: true },
+        { header: 'Labels' },
+        {
+          action: 'mdi-label',
+          title: 'Family'
+        },
+        {
+          action: 'mdi-alert-octagon',
+          title: 'Spam'
+        },
+        { divider: true },
+        { header: 'Labels' },
+        {
+          action: 'mdi-label',
+          title: 'Family'
+        },
+        {
+          action: 'mdi-label',
+          title: 'Friends'
+        },
+        {
+          action: 'mdi-label',
+          title: 'Work'
+        }
+      ]
+    }
+  }
 }
+
 </script>
 
 <style>
